f4028cca7f1cd5d307175b371146b2de60d2f1d5
Niamh Armour <C22397066@mytudublin.ie>
2025-11-25 11:37:57 +0000

    fixing the folder structure and starting the register page

---
+++ app/controllers/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const sql = require('./db.js');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const validator = require('validator');
+const bodyParser = require('body-parser');
+
+const app = express();
+const saltRounds = 10;
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'your-production-secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+app.use(cors());
+
+// Serve all static files (CSS, JS, images)
+app.use(express.static(path.join(__dirname, '../views')));
+
+// HTML routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/register.html'));
+});
+
+// Start server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
